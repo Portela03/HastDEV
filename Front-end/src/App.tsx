b8e6d2a4e19d5bffa335a12b1f92d02934dd9585
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-10-03 00:00:38 -0300

    Organizacao do Layout e Roteamento.

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import About from './layout/About/About';
+import ContactUs from './layout/ContactUs/ContactUs';
+import Footer from './layout/Footer/Footer';
+import Header from './layout/Header/Header';
+import Hero from './layout/Hero/Hero';
+import Login from './layout/Login/Login';
+import Project from './layout/Project/Project';
+import Register from './layout/Register/Register';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
---
+++ Front-end/src/layout/Register/Register.tsx
@@ -0,0 +1,5 @@
+const Register: React.FC = () => {
+  return <div>Registro</div>;
+};
+
+export default Register;
