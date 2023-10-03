98b70e2b772d1bb5277fe9d85ef7b584509b6d0a
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-10-03 10:40:56 -0300

    Modelo Base do Header;

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -14,7 +14,6 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
         <Header />
         <Routes>
           <Route path="/" element={<Hero />} />
@@ -25,7 +24,6 @@ function App() {
           <Route path="/project" element={<Project />} />
         </Routes>
         <Footer />
-      </div>
     </Router>
   )
 }
--- Front-end/src/layout/Header/Header.tsx
+++ Front-end/src/layout/Header/Header.tsx
@@ -1,5 +1,46 @@
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import LogoLight from "../../assets/LogoLight.svg";
+
 const Header: React.FC = () => {
-    return <div>Header</div>;
-  };
-  
-  export default Header;
+  return (
+    <Navbar expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#" className="mr-auto">
+          <img src={LogoLight} alt="Logo HastyDEV modo Light" className="mt-2"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="ml-auto topnav w-100 justify-content-between">
+            <div
+              className="d-flex align-items-center gap-4"
+              style={{ marginLeft: 100 }}
+            >
+              <Nav.Item>
+                <Nav.Link href="#">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#">Sobre Nós</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#">Contate-nos</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#">O Projeto</Nav.Link>
+              </Nav.Item>
+            </div>
+            <div className="d-flex align-items-center gap-3">
+              <Nav.Item>
+                <Button variant="primary">Login</Button>
+              </Nav.Item>
+              <Nav.Item>
+                <Button variant="danger">Inscreva-se</Button>
+              </Nav.Item>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
