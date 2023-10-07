3f19584228c7b6450e2429194a65f6565309fbc2
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-10-06 21:51:43 -0300

    Criação da estrutura da página About

--- Front-end/src/layout/Hero/Hero.tsx
+++ Front-end/src/layout/Hero/Hero.tsx
@@ -7,25 +7,29 @@ import HeroImg4 from "../../assets/images/Hero/HeroImg4.webp";
 const Hero: React.FC = () => {
   return (
     <Container className="text-center">
-      <h1 className="display-3">Bem-vindo à HastyDEV - Desenvolva Projetos Reais</h1>
+      <h1 className="display-3">
+        Bem-vindo à HastyDEV - Desenvolva Projetos Reais
+      </h1>
       <p className="lead">
-        Encontre Projetos Reais para Anexar ao seu Portfólio, Conquiste Seu Local no Mercado E Seja Recompensado Por Isso!
+        Encontre Projetos Reais para Anexar ao seu Portfólio, Conquiste Seu
+        Local no Mercado E Seja Recompensado Por Isso!
       </p>
-      <Button href="/register" variant="primary">Inscreva-se</Button>
-
+      <Button href="/register" variant="primary">
+        Inscreva-se
+      </Button>
       <div className="mt-4">
         <Row>
           <Col md={3}>
-            <img src={HeroImg1} alt="" className="img-fluid"/>
+            <img src={HeroImg1} alt="" className="img-fluid" />
           </Col>
           <Col md={3}>
-            <img src={HeroImg2} alt="" className="img-fluid"/>
+            <img src={HeroImg2} alt="" className="img-fluid" />
           </Col>
           <Col md={3}>
-            <img src={HeroImg3} alt="" className="img-fluid"/>
+            <img src={HeroImg3} alt="" className="img-fluid" />
           </Col>
           <Col md={3}>
-            <img src={HeroImg4} alt="" className="img-fluid"/>
+            <img src={HeroImg4} alt="" className="img-fluid" />
           </Col>
         </Row>
       </div>
