c46fd1b649b65346a4bec819bbfaa846615d4da7
lucasqc04 <lucasqcampos9@gmail.com>
2023-10-11 22:14:58 -0300

    modelo de usuario feito usando sequelize

--- Back-end/User/models/userModel.js
+++ Back-end/User/models/userModel.js
@@ -1,1 +1,35 @@
-// Pode definir uma classe para o usuario aqui
+const Sequelize = require('sequelize');
+
+// Definir o modelo de usuário
+const User = sequelize.define('user', {
+  userid: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: Sequelize.STRING(45),
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING(200),
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING(45),
+    allowNull: false,
+  },
+  first_name: {
+    type: Sequelize.STRING(45),
+    allowNull: false,
+  },
+  last_name: {
+    type: Sequelize.STRING(45),
+    allowNull: false,
+  },
+}, {
+  timestamps: false, 
+});
+
+module.exports = User;
