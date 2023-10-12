5994b15fcc37ac7b70c87726fa43c301686bd74c
lucasqc04 <lucasqcampos9@gmail.com>
2023-10-11 22:04:40 -0300

    organização de pastas e das mensagens de erro

--- Back-end/Db.js
+++ Back-end/Db.js
@@ -4,10 +4,10 @@ const pino = require('pino');
 const logger = pino();
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "users",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 
